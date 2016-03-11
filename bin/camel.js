f281196ea026a73e0518e4c60164e97b16f81680
mhailstone <matthew.hailstone@gmail.com>
2016-03-11 12:26:12 -0700

    Fixed bug with str

--- bin/camel.js
+++ bin/camel.js
@@ -7,12 +7,12 @@ exports.to = function(sep, text) {
     var rx = /[A-Z]/g;
     var subStr;
 
-    while (match = rx.exec(str)) {
-        subStr = str.substring(index, match.index).toLowerCase();
+    while (match = rx.exec(text)) {
+        subStr = text.substring(index, match.index).toLowerCase();
         if (subStr) result += subStr + sep;
         index = match.index;
         console.log(match);
     }
 
-    return result + str.substr(index).toLowerCase();
+    return result + text.substr(index).toLowerCase();
 };
