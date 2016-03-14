aed79d4fbe676b9221fe99928c391e6d897b3e8f
Gi60s <djK82hR#!E%KPpd>
2016-03-14 15:37:16 -0600

    use endpoint data

--- bin/server/favicon.js
+++ bin/server/favicon.js
@@ -3,12 +3,13 @@ const favicon       = require('express-favicon');
 const noop          = require('./noop');
 const path          = require('path');
 
-module.exports = function(config, stats) {
-    return config.proxy ? noop : middleware(config, stats);
+module.exports = function() {
+    return function(req, res, next) {
+        if (!req.proxy && /\/favicon\.ico$/.test(req.url) && req.filePath) {
+            res.set('Content-Type', 'image/x-icon');
+            res.sendFile(req.filePath);
+        } else {
+            next();
+        }
+    };
 };
-
-function middleware(config, stats) {
-    var faviconPath = path.resolve(config.src, 'favicon.ico');
-    if (!stats[faviconPath]) faviconPath = path.resolve(__dirname, '../www/favicon.ico');
-    return favicon(faviconPath);
-}
