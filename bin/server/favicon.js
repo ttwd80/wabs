8b2fee0b25d56f13d5da734b508f364611eb7fb3
James Speirs <james.speirs@gmail.com>
2016-03-16 16:30:32 -0600

    fix file exists check

--- bin/server/favicon.js
+++ bin/server/favicon.js
@@ -2,7 +2,7 @@
 
 module.exports = function(stats) {
     return function(req, res, next) {
-        if (!req.wabs.proxy && /\/favicon\.ico$/.test(req.url) && stats.get(req.wabs.fsStat) !== null) {
+        if (!req.wabs.proxy && /\/favicon\.ico$/.test(req.url) && stats.get(req.wabs.fsStat)) {
             res.set('Content-Type', 'image/x-icon');
             res.sendFile(req.wabs.fsStat.path);
         } else {
