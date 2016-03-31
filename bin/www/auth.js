dea2d5552db5ffbe8d21a1caf166130d77ae9853
James Speirs <james.speirs@gmail.com>
2016-03-31 11:47:48 -0600

    fix negative auto refresh timeout

--- bin/www/auth.js
+++ bin/www/auth.js
@@ -243,7 +243,7 @@
     function setAutoRefreshTimeout() {
         clearTimeout(autoRefreshTimeoutId);
         if (autoRefresh < 0) {
-            autoRefreshTimeoutId = setTimeout(auth.refresh, expires - (autoRefresh * 1000));
+            autoRefreshTimeoutId = setTimeout(auth.refresh, expires + (autoRefresh * 1000));
         } else if (autoRefresh > 0) {
             autoRefreshTimeoutId = setTimeout(auth.refresh, autoRefresh * 1000);
         }
