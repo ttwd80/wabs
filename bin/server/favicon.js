f5a1c12bfe5bc1572806b8842bd0ac1763720bd6
James Speirs <james.speirs@gmail.com>
2016-04-27 13:12:52 -0600

    check for GET method

--- bin/server/favicon.js
+++ bin/server/favicon.js
@@ -2,7 +2,7 @@
 
 module.exports = function(stats) {
     return function favicon(req, res, next) {
-        if (!req.wabs.proxy && /\/favicon\.ico$/.test(req.url) && stats.get(req.wabs.fsStat)) {
+        if (!req.wabs.proxy && req.method === 'GET' && /\/favicon\.ico$/.test(req.url) && stats.get(req.wabs.fsStat)) {
             res.set('Content-Type', 'image/x-icon');
             res.sendFile(req.wabs.fsStat.path);
         } else {
