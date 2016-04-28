32805ddc5e8e8048fa38fdaa56bca21a30c1547c
James Speirs <james.speirs@gmail.com>
2016-04-28 13:51:56 -0600

    add mock request functionality

--- bin/request.js
+++ bin/request.js
@@ -1,11 +1,64 @@
 "use strict";
 var request             = require('request');
 
-module.exports = function(options) {
+module.exports = Request;
+
+function Request(options) {
     return new Promise(function(resolve, reject) {
-        request(options, function(err, response, body) {
-            if (err) return reject(err);
-            return resolve({ response: response, body: body });
-        });
+        const mockResult = Request.mock.execute(options);
+        if (mockResult) {
+            resolve(mockResult);
+        } else {
+            request(options, function(err, response, body) {
+                if (err) return reject(err);
+                return resolve({ response: response, body: body });
+            });
+        }
     });
-};
+}
+
+Request.mock = (function() {
+    const factory = {};
+    let store = [];
+
+    factory.execute = function(options) {
+        for (let i = 0; i < store.length; i++) {
+            let item = store[i];
+            if (item.filter(options)) {
+                if (item.once) store.splice(i, 1);
+                return item.response(options);
+            }
+        }
+    };
+
+    factory.off = function(filter) {
+        for (let i = 0; i < store.length; i++) {
+            if (store[i].filter === filter) {
+                store.splice(i, 1);
+                return;
+            }
+        }
+    };
+
+    factory.on = function(filter, response) {
+        store.push({
+            once: false,
+            filter: filter,
+            response: response
+        });
+    };
+
+    factory.once = function(filter, response) {
+        store.push({
+            once: true,
+            filter: filter,
+            response: response
+        });
+    };
+
+    factory.reset = function() {
+        store = [];
+    };
+
+    return factory;
+})();
